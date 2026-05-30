9206510ec9997bd1363a4b3d061654f100a94c49
Nam134206 <namp90541@gmail.com>
2026-05-30 18:30:52 +0700

    [EVENT] Add input onChange handlers

--- sesstion_04_react_basics/projects/portfolio_react/src/components/Contact.jsx
+++ sesstion_04_react_basics/projects/portfolio_react/src/components/Contact.jsx
@@ -11,3 +11,19 @@ function Contact() {
 
     // Error state
     const [errors, setErrors] = useState({});
+    // Handle input changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+
+        // Clear error when user types
+        if (errors[name]) {
+            setErrors(prev => ({
+                ...prev,
+                [name]: ''
+            }));
+        }
+    };
